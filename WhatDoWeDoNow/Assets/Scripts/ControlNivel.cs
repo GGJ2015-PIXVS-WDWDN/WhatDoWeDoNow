7b0af2215398cf51e662da02b412c7eddeb30335
Juana Paulina Aguila Hernandez <j.paulina.aguila.hernandez@hotmail.com>
2015-01-24 02:53:10 -0600

    Creación de control de nivel.

---
+++ WhatDoWeDoNow/Assets/Scripts/ControlNivel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlNivel : MonoBehaviour {
+
+	public GameObject Player;
+	public GameObject PosicionInicial;
+
+	// Use this for initialization
+	void Start () {
+		PosicionarJugador();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PosicionarJugador()
+	{
+		Instantiate(Player, PosicionInicial.transform.position, Quaternion.identity);
+	}
+
+}
