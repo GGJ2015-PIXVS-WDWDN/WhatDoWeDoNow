76930ece846c248f29e6dc07b0efcf715646df8d
Juana Paulina Aguila Hernandez <j.paulina.aguila.hernandez@hotmail.com>
2015-01-24 07:09:15 -0600

    Modificación de primer escenario.

---
+++ WhatDoWeDoNow/Assets/Scripts/ControlCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlCamera : MonoBehaviour {
+
+	public GameObject Player;
+	public float Velocidad;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(Player.transform.position.x,
+		                                 transform.position.y,
+		                                 transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
