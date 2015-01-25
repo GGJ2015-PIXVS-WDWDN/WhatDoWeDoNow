264d5cfe7d4d169fb6c10676e750abd7a4f17625
Juana Paulina Aguila Hernandez <j.paulina.aguila.hernandez@hotmail.com>
2015-01-25 14:43:10 -0600

    Cambio de escenas.

--- GlobalGameJam/Assets/Scripts/Vida.cs
+++ GlobalGameJam/Assets/Scripts/Vida.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
 public class Vida : MonoBehaviour {
 
 	public int puntuacion = 100;
 	public TextMesh marcador;
+	public GameObject jugador;
+	public GameObject cruz;
+	public GUIStyle estiloGUI;
 
 	// Use this for initialization
 	void Start () {
 		NotificationCenter.DefaultCenter ().AddObserver (this, "DisminuyePuntos");
 		ActualizarMarcador ();
+		jugador.SetActive(true);
+		cruz.SetActive(false);
+
 	}
 
 	void DisminuyePuntos(Notification notificacion)
@@ -29,9 +36,32 @@ public class Vida : MonoBehaviour {
 
 		if (puntuacion == 0) 
 		{
+
+			Debug.Log("Perdi");
 			puntuacion = 0;
+
+			cruz.transform.position = new Vector3(jugador.transform.position.x,
+			                                 0.0f,
+			                                 jugador.transform.position.z);
+			jugador.SetActive(false);
+			cruz.SetActive(true);
+
+		}
+
+	}
+
+	void OnGUI()
+	{
+		if (puntuacion == 0) 
+		{
+			GUI.Label(new Rect(400.0f,100.0f,100.0f,100.0f), "Game Over", estiloGUI);
 			//Debug.Break();
-			Application.LoadLevel(3);
+			//Se cambia la papa por una cruz
+			if(GUI.Button(new Rect(600.0f,300.0f,100.0f,100.0f), "¿Seguir Jugando?", estiloGUI))
+			{
+				Debug.Log("Haz seleccionado seguir jugando.");
+				Application.LoadLevel(2);
+			}
 		}
 
 	}
