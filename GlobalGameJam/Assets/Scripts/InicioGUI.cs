343088762d42226e06754e9ba732f878fbc3d65e
Juana Paulina Aguila Hernandez <j.paulina.aguila.hernandez@hotmail.com>
2015-01-25 07:43:35 -0600

    Modificación de escenas.

--- GlobalGameJam/Assets/Scripts/InicioGUI.cs
+++ GlobalGameJam/Assets/Scripts/InicioGUI.cs
@@ -7,12 +7,9 @@ public class InicioGUI : MonoBehaviour
 
 	public Button[] botones;
 	private int cantidadBotones;
-	//public int[] nivelCarga;
-	
-	//public GUIStyle estilo;
-	//public GUISkin skin;
-	//public GenericButton[] botones;
-	
+
+	public GUIStyle estiloGUI;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -29,11 +26,9 @@ public class InicioGUI : MonoBehaviour
 	void OnGUI()
 	{
 
-		//GUI.Label(new Rect(250,100,100,100), "Revienta las esferas!", estilo);
-
 		for (int i = 0; i< this.cantidadBotones; i++)
 		{
-			if(GUI.Button(new Rect(botones[i].x,botones[i].y,botones[i].sizeX ,botones[i].sizeY ) , botones[i].title ))
+			if(GUI.Button(new Rect(botones[i].x,botones[i].y,botones[i].sizeX ,botones[i].sizeY ) , botones[i].title, estiloGUI))
 			{
 				Debug.Log("Cargar el nivel " + botones[i].title);
 				Application.LoadLevel(botones[i].escena);
@@ -41,17 +36,5 @@ public class InicioGUI : MonoBehaviour
 		}
 
 
-		//if(GUI.Button(new Rect(300,150,100,50) , "Inicio"))
-		//{
-		//	Debug.Log("Cargar el nivel " + nivelCarga);
-		//	Application.LoadLevel(nivelCarga[0]);
-		//}
-		
-		//if(GUI.Button(new Rect(300,220,100,50) , "Instrucciones"))
-		//{
-		//	Debug.Log("Cargar el nivel " + nivelCarga);
-		//	Application.LoadLevel(nivelCarga[1]);
-		//}	
-		
 	}
 }
